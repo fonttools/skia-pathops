7921ed37b28eb56d31459608ef0525bcaa52abd3
Cosimo Lupo <cosimo@anthrotype.com>
2017-11-07 22:22:59 +0000

    Add Travis CI and multibuild
    https://github.com/matthew-brett/multibuild/

---
+++ config.sh
@@ -0,0 +1,13 @@
+# Define custom utilities
+# Test for OSX with [ -n "$IS_OSX" ]
+
+function pre_build {
+    # Any stuff that you need to do before you start building the wheels
+    # Runs in the root directory of this repository.
+    :
+}
+
+function run_tests {
+    # The function is called from an empty temporary directory.
+    python -c "import skia"
+}
