0aa162391da5a83a555422eb8d62ecfeea8259b6
Cosimo Lupo <cosimo@anthrotype.com>
2017-11-08 19:40:49 +0000

    [config.sh] run tests with tox in multibuild docker container

--- config.sh
+++ config.sh
@@ -9,5 +9,26 @@ function pre_build {
 
 function run_tests {
     # The function is called from an empty temporary directory.
-    python -c "import skia"
+    # Get absolute path to the pre-compiled wheel
+    wheelhouse=$(abspath ../wheelhouse)
+    wheel=`ls ${wheelhouse}/skia*.whl | head -n 1`
+
+    # select tox environment
+    if [ -n "$IS_OSX" ]; then
+        PYTHON_VERSION=$MB_PYTHON_VERSION
+    fi
+    case "${PYTHON_VERSION}" in
+        2.7)
+           TOXENV=py27
+           ;;
+        3.5)
+           TOXENV=py35
+           ;;
+        3.6)
+           TOXENV=py36
+           ;;
+    esac
+
+    # Runs tests on installed wheel
+    tox --installpkg $wheel -e $TOXENV
 }
