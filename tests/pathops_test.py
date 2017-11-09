1904a49cf55bb7471d8658dcfb163f5ee927559b
Cosimo Lupo <cosimo@anthrotype.com>
2017-11-09 22:55:50 +0000

    [pathops_test] add simple tests for Path class

--- tests/pathops_test.py
+++ tests/pathops_test.py
@@ -1,5 +1,36 @@
 from pathops import _pathops
+from pathops import Path, PathPen
+
 
 def test_demo():
     assert _pathops.demo()
+
+
+class PathTest(object):
+
+    def test_init(self):
+        path = Path()
+        assert isinstance(path, Path)
+
+    def test_getPen(self):
+        path = Path()
+        pen = path.getPen()
+        assert isinstance(pen, PathPen)
+
+    def test_copy(self):
+        path1 = Path()
+        path2 = Path(path1)
+        # TODO expose operator== to check for equality
+
+    def test_draw(self):
+        path = Path()
+        pen = path.getPen()
+        pen.moveTo((0, 0))
+        pen.lineTo((1.0, 2.0))
+        pen.curveTo((3.5, 4), (5, 6), (7, 8))
+        pen.qCurveTo((9, 10), (11, 12))
+        pen.closePath()
+
+        path2 = Path()
+        path.draw(path2.getPen())
 
