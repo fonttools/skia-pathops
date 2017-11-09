73fea84c5c5d615a5bb98ee170f540e966130881
Cosimo Lupo <cosimo@anthrotype.com>
2017-11-09 22:44:59 +0000

    WIP: add Path and PathPen python extension types
    The Path class works like a FontTools glyph; has a getPen method that returns
    a pen instance that can draw on that path. The pen has the same interface as
    FontTools BasePen.

--- src/python/pathops/_pathops.pyx
+++ src/python/pathops/_pathops.pyx
@@ -12,6 +12,90 @@ from ._skia.core cimport (
 from ._skia.pathops cimport SkOpBuilder, kUnion_SkPathOp
 
 
+cdef class Path:
+
+    cdef SkPath path
+    cdef PathPen pen
+
+    def __cinit__(self):
+        self.pen = PathPen(self)
+
+    def __init__(self, other_path=None):
+        if other_path is None:
+            return
+        cdef Path static_path = other_path
+        self.path = SkPath(static_path.path)
+
+    def getPen(self):
+        return self.pen
+
+    def draw(self, pen):
+        cdef SkPoint p[4]
+        cdef SkPath.Verb verb
+        cdef SkPath.Iter iterator = SkPath.Iter(self.path, False)
+
+        verb = iterator.next(p, False)
+        while verb != kDone_Verb:
+            if verb == kMove_Verb:
+                pen.moveTo((p[0].x(), p[0].y()))
+            elif verb == kLine_Verb:
+                pen.lineTo((p[1].x(), p[1].y()))
+            elif verb == kCubic_Verb:
+                pen.curveTo(
+                    (p[1].x(), p[1].y()),
+                    (p[2].x(), p[2].y()),
+                    (p[3].x(), p[3].y()))
+            elif verb == kQuad_Verb:
+                pen.qCurveTo(
+                    (p[1].x(), p[1].y()),
+                    (p[2].x(), p[2].y()))
+            elif verb == kConic_Verb:
+                raise ValueError("conic is unsupported")
+            elif verb == kClose_Verb:
+                pen.closePath()
+            verb = iterator.next(p, False)
+
+    def dump(self):
+        # prints a text repesentation of SkPath to stdout
+        self.path.dump()
+
+
+cdef class PathPen:
+
+    cdef SkPath *path_ptr
+
+    def __cinit__(self, Path path):
+        self.path_ptr = &path.path
+
+    cpdef moveTo(self, pt):
+        self.path_ptr.moveTo(pt[0], pt[1])
+
+    cpdef lineTo(self, pt):
+        self.path_ptr.lineTo(pt[0], pt[1])
+
+    cpdef curveTo(self, pt1, pt2, pt3):
+        # support BasePen "super-beziers"? Nah.
+        self.path_ptr.cubicTo(
+            pt1[0], pt1[1],
+            pt2[0], pt2[1],
+            pt3[0], pt3[1])
+
+    cpdef qCurveTo(self, pt1, pt2):
+        # TODO TrueType spline can have multiple offcurves, split
+        # into atomic quadratic curves
+        self.path_ptr.quadTo(pt1[0], pt1[1], pt2[0], pt2[1])
+
+    cpdef closePath(self):
+        self.path_ptr.close()
+
+    cpdef endPath(self):
+        # XXX can a SkPath be "open"? How does SkOpBuilder handles them?
+        pass
+
+    cpdef addComponent(self, glyphName, transformation):
+        pass
+
+
 cpdef int demo():
     cdef SkOpBuilder builder
     cdef SkPath path1, path2, result
